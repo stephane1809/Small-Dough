8533fb3b33b2550b99faf4b0108d1f38a3917f37
Stephane <stephanegiraolinhares@gmail.com>
2023-04-10 19:50:53 -0300

    first commit

---
+++ Small Dough/Small Dough/Small_DoughApp.swift
@@ -0,0 +1,17 @@
+//
+//  Small_DoughApp.swift
+//  Small Dough
+//
+//  Created by Stephane Girão Linhares on 10/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Small_DoughApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
