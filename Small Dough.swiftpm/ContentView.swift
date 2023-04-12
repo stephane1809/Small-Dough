7d100a1f3b2a582d6e1808ac55e702812e9fdc12
Stephane <stephanegiraolinhares@gmail.com>
2023-04-12 15:34:57 -0300

    duplicando a massa quando clica

--- Small Dough.swiftpm/ContentView.swift
+++ Small Dough.swiftpm/ContentView.swift
@@ -1,18 +1,13 @@
 import SwiftUI
+import SpriteKit
 
 struct ContentView: View {
+
+    @State var scene: GameScene = .makeFullscreenScene()
+
     var body: some View {
         VStack {
-            Image("massa")
-            Button{
-                print("oi")
-
-            } label: {
-                Image(systemName: "pause.fill")
-                    .foregroundColor(.black)
-                    .frame(width: 100, height: 100, alignment: .center)
-            }
-
+            SpriteView(scene: scene)
         }
     }
 }
